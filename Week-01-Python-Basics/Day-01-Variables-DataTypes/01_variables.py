0a46ecd97702c87e8a435217a3c10361c4e94fa9
Tariq Mehmood <tariqmehmoodsthp23@gmail.com>
2026-07-06 10:04:12 +0500

    Define and print string, integer, float, and boolean
    ===========================================
    Week 01 - Day 01
    Topic: Variables
    
    Author: Tariq
    
    Goal:
    - Learn Variables
    - Store Data
    - Print Variables
    ===========================================

--- Week-01-Python-Basics/Day-01-Variables-DataTypes/01_variables.py
+++ Week-01-Python-Basics/Day-01-Variables-DataTypes/01_variables.py
@@ -1,1 +1,17 @@
 print("Hello Tariq")
+# String Variable
+name = "Tariq Mehmood"
+
+# Integer Variable
+age = 23
+
+# Float Variable
+height = 5.5
+
+# Boolean Variable
+is_student = True
+
+print(name)
+print(age)
+print(height)
+print(is_student)
