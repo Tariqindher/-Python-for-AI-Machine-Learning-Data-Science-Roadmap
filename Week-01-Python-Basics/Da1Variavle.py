048c64017bcdbbbced710510d3d0ef80700b5216
Tariq Mehmood <tariqmehmoodsthp23@gmail.com>
2026-07-07 09:32:35 +0500

    Create Da1Variavle.py with variable examples
    Added a Python script demonstrating variable types.

---
+++ Week-01-Python-Basics/Da1Variavle.py
@@ -0,0 +1,22 @@
+print("Hello Tariq")
+# String Variable
+name = "Tariq Mehmood"
+
+# Integer Variable
+age = 23
+
+# Float Variable
+height = 5.5
+
+# Boolean Variable
+is_student = True
+
+print(name)
+print(age)
+print(height)
+print(is_student)
+city = "Sukkur"
+country = "Pakistan"
+
+print(city)
+print(country)
