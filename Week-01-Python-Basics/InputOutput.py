9faa710a5b9637e4ff757fdff7110c4908b1ac50
Tariq Mehmood <tariqmehmoodsthp23@gmail.com>
2026-07-07 09:51:07 +0500

    InputOutput.py with print examples
    Fix print statements and add formatted output examples.

---
+++ Week-01-Python-Basics/InputOutput.py
@@ -0,0 +1,20 @@
+name=input("Enter your name")
+
+city=input("Enter your City Name : ")
+age=int(input(" Enter your age: "))
+heighet=float(input("Enter your heighet"))
+print("Name",name)
+print("The City is : " ,city)
+print("Your age is :  ", age)
+print("Your heighet is: ", heighet)
+
+#Output type
+print("Hi")
+print(name)
+print(f"My name is {name} and my age is {age} ")
+# .format() Method
+print(f"My name is {} and my age is {} ".format(name, age)
+
+
+
+
