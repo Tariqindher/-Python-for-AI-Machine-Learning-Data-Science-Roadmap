ff907f907c20a893668561b9d9b91f56a1a1d119
Tariq Mehmood <tariqmehmoodsthp23@gmail.com>
2026-07-09 16:34:43 +0500

    Enhance Error Handling
    Added error handling examples with try, except, else, and finally blocks. Included age validation and multiple exception handling.

--- Week-01-Python-Basics/ErrorHandling.py
+++ Week-01-Python-Basics/ErrorHandling.py
@@ -18,3 +18,40 @@ finally:
 #3. Else Block :   The else block runs only if no exception occurs.
 #4. Finally Block: The finally block always executes, whether an exception occurs or not.
 #5. Raised Statement :The raise statement is used to create (raise) an exception manually.
+
+#if the user entered number it run else block otherwise run except block 
+try:
+    num=int(input("Please enter the Number : "))
+    #if user enter abc it raises a ValueError and goes to except
+except ValueError:
+    print("Invalid input. Please enter a number")
+else: 
+    print("You  entered: ",num)
+finally:
+    print("It is always run ")
+#Raise Statemnt
+age = int(input("Enter your age"))
+
+if age < 18:
+    raise ValueError("Age must be 18 or older ")
+print("This Code will run  when age>=18.")
+
+#Multiple exception examples
+try:
+    num = int(input("Enter a number: "))
+    result = 10 / num
+
+except ValueError:
+    print("Please enter numbers only.")
+
+except ZeroDivisionError:
+    print("Cannot divide by zero.")
+
+else:
+    print("Result =", result)
+
+finally:
+    print("Program finished.")
+
+
+
