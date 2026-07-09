04cf38da731967e9d9d9bfeb3e0a23f31cce1661
Tariq Mehmood <tariqmehmoodsthp23@gmail.com>
2026-07-09 15:37:52 +0500

    basic operators demonstration

---
+++ Week-01-Python-Basics/Operators.py
@@ -0,0 +1,21 @@
+a = 10
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+age = 18
+
+print(age == 18)
+print(age > 20)
+print(age != 15)
+
+age = 20
+
+print(age > 18 and age < 30)
+print(age > 25 or age < 30)
+print(not(age > 18))
