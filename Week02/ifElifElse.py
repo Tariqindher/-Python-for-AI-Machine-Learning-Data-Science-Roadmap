650f38da5c39af0660c56f90b20df9ffdabb1add
Tariq Mehmood <tariqmehmoodsthp23@gmail.com>
2026-07-17 16:57:01 +0500

    Implement grading system with input validation
    Added input validation and grading logic for marks.

--- Week02/ifElifElse.py
+++ Week02/ifElifElse.py
@@ -1,1 +1,33 @@
-print("hi")
+#Concepts Covered:
+#✅ if
+#✅ elif
+#✅ else
+#✅ Nested if
+try:
+    marks = int(input("Enter Marks (0-100): "))
+
+    if marks < 0 or marks > 100:
+        print("Invalid Marks")
+        exit()
+
+except ValueError:
+    print("Invalid Marks")
+    exit()
+
+if marks >= 40:
+    print("Pass")
+
+    if marks >= 90:
+        print("Grade: A")
+    elif marks >= 80:
+        print("Grade: B")
+    elif marks >= 70:
+        print("Grade: C")
+    elif marks >= 60:
+        print("Grade: D")
+    else:  # 40–59
+        print("Grade: E")
+
+else:
+    print("Fail")
+    print("Grade: F")
